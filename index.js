d6f9ee088cd3307451d724329528c74b73b1fd9c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-01-15 16:24:40 +0800

    [feature] add auth code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import BaseMethods from "./src/utils/data/checker";
-import Validate from "./src/utils/data/validate"
+import Validate from "./src/utils/data/validate";
+import Auth from "./src/utils/data/auth";
 
 export default {
     BaseMethods,
-    Validate
+    Validate,
+    Auth
 }
