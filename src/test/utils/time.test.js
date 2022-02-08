6a310aaa21eb5b4b57632908d6a395c14a2149ad
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-02-08 21:38:48 +0800

    [feature] tweak

--- src/test/utils/time.test.js
+++ src/test/utils/time.test.js
@@ -1,9 +1,6 @@
 
-const getMonthStart = require('../utils/time/TimeUtils/getMonthStart');
-//const sum = require('.');
 
 test('adds 1 + 2 to equal 3', () => {
-  let monthStart = getMonthStart(new Date());
 
 
 });
