d686028791f81230a26cb51afaec75abb602799d
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-02-08 14:50:34 +0800

    [feature] tweak

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 //import BaseMethods from "./src/utils/data/checker";
 //import Validate from "./src/utils/data/validate";
-import  ResponseHandler  from "./src/net/rest/ResponseHandler";
+//import  ResponseHandler  from "./src/net/rest/ResponseHandler";
 
-export default {
-    //BaseMethods,
-    //Validate,
-    ResponseHandler
-}
+//export default ResponseHandler
+
