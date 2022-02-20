5ef2453ebb9b12131a41ee6f101f90ee65c75f26
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-02-20 13:36:54 +0800

    [feature] tweak

--- src/model/immutable/WheelGlobal.ts
+++ src/model/immutable/WheelGlobal.ts
@@ -3,7 +3,7 @@ export class WheelGlobal{
     public static readonly MOUSE_CLICK: any = "click";
     public static readonly ACCESS_TOKEN_NAME: string = "x-access-token";
     public static readonly REFRESH_TOKEN_NAME: string = "x-refresh-token";
-    public static readonly REDDWARF_APP_ID_KEY: string = "appId";
+    public static readonly REDDWARF_APP_ID_KEY: string = "app-id";
     public static readonly USER_NAME: string = "username";
     public static readonly PASSWORD: string = "password";
     public static readonly BASE_AUTH_URL: string = "base-auth-url";
