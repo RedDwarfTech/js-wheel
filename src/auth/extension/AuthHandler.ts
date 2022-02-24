5b17126d05cbe6b159e9d56e1edd77fb92cca4e6
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-02-24 22:41:40 +0800

    [tweak] tweak then to await

--- src/auth/extension/AuthHandler.ts
+++ src/auth/extension/AuthHandler.ts
@@ -5,10 +5,10 @@ import DeviceHandler from "@utils/data/DeviceHandler";
 
 export const AuthHandler = {
     pluginLogin: async () => {
-        let username:string = await LocalStorage.readLocalStorage(WheelGlobal.USER_NAME);
-        let password:string = await LocalStorage.readLocalStorage(WheelGlobal.PASSWORD);
-        let deviceId:string = await DeviceHandler.getDeviceId();
-        let appId:string = await LocalStorage.readLocalStorage(WheelGlobal.REDDWARF_APP_ID_KEY);
+        let username: string = await LocalStorage.readLocalStorage(WheelGlobal.USER_NAME);
+        let password: string = await LocalStorage.readLocalStorage(WheelGlobal.PASSWORD);
+        let deviceId: string = await DeviceHandler.getDeviceId();
+        let appId: string = await LocalStorage.readLocalStorage(WheelGlobal.REDDWARF_APP_ID_KEY);
         let loginParams = {
             phone: username,
             password: password,
@@ -23,29 +23,24 @@ export const AuthHandler = {
         const baseAuthUrl = await LocalStorage.readLocalStorage(WheelGlobal.BASE_AUTH_URL);
         const userLoginUrl = await LocalStorage.readLocalStorage(WheelGlobal.USER_LOGIN_URL);
         const baseUrl = baseAuthUrl + userLoginUrl;
-        fetch(baseUrl, {
+        let response = await fetch(baseUrl, {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json',
             },
             body: JSON.stringify(params),
-        })
-            .then((response:any) => {
-                let res = response.json();
-                if (res && res.result && res.result.accessToken) {
-                    const accessToken = res.result.accessToken;
-                    const refreshToken = res.result.refreshToken;
-                    chrome.storage.local.set(
-                        {
-                            [WheelGlobal.ACCESS_TOKEN_NAME]: accessToken,
-                            [WheelGlobal.REFRESH_TOKEN_NAME]: refreshToken,
-                        }
-                    );
+        });
+        let res = await response.json();
+        if (res && res.result && res.result.accessToken) {
+            const accessToken = res.result.accessToken;
+            const refreshToken = res.result.refreshToken;
+            chrome.storage.local.set(
+                {
+                    [WheelGlobal.ACCESS_TOKEN_NAME]: accessToken,
+                    [WheelGlobal.REFRESH_TOKEN_NAME]: refreshToken,
                 }
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+            );
+        }
     },
 }
 
