5638548ec2e08ccef423f4a7de8eb973c1c9cab0
jiang qiang <jiangtingqiang@gmail.com>
2022-03-02 20:53:55 +0800

    [feature] add unit test code

---
+++ dist/src/test/utils/time.test.js
@@ -0,0 +1,10 @@
+import DeviceHandler from "../../utils/data/DeviceHandler";
+import { describe } from 'mocha';
+describe('TaxCalculator', function () {
+    describe('test calculateTaxAmount(salesPrice, taxRate)', function () {
+        it('should calculate the tax for an item', function () {
+            // let xxxx = await DeviceHandler.getDeviceId();
+            // expect(xxxx === "xxxx")
+        });
+    });
+});
---
+++ src/test/utils/time.test.ts
@@ -0,0 +1,15 @@
+import { describe } from 'mocha';
+import { expect } from 'chai';
+
+describe('TaxCalculator', () =>
+{
+    describe('test calculateTaxAmount(salesPrice, taxRate)', () =>
+    {
+        it('should calculate the tax for an item', () =>
+        {
+            // let xxxx = await DeviceHandler.getDeviceId();
+            // expect(xxxx === "xxxx")
+        });
+    });
+});
+
