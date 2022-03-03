2026cefdee3a2960488567c7fa773c0c2b59aa4b
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-03-04 00:07:16 +0800

    [feature] add unit test code

--- test/utils/data/DeviceHandler.test.ts
+++ test/utils/data/DeviceHandler.test.ts
@@ -5,6 +5,10 @@ import DeviceHandler from '@utils/data/DeviceHandler'
 describe('Device Test', function () {
     it('Get device fingerprints', async function () {
        let fingerprints:string = await DeviceHandler.getDeviceId();
-       expect(fingerprints).to.be.a("d");
+       expect(fingerprints).to.be.equal("xxxx");
     })
+    it('Get device fingerprints enhance', async function () {
+        let fingerprints:string = await DeviceHandler.getDeviceIdEnhance();
+        expect(fingerprints).to.be.a("string");
+     })
 })
--- test/utils/time.test.ts
+++ test/utils/time.test.ts
@@ -1,9 +1,10 @@
-var expect1 = require('chai').expect;
+var expect = require('chai').expect;
 import DeviceHandler from "@utils/data/DeviceHandler";
 
 describe('加法函数的测试', function() {
-  it('1 加 1 应该等于 2', function() {
-    expect1(1+ 1).to.be.equal(2);
-    DeviceHandler.getDeviceId()
+  it('1 加 1 应该等于 2', async function() {
+    expect(1+ 1).to.be.equal(2);
+    let deviceId:string = await DeviceHandler.getDeviceId();
+    expect(deviceId).to.be.equal("xxxx");
   });
 });
