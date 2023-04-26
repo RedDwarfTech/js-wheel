7b1a82d3ef271a9ed604337ffdbfef7d1beb3939
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-04-26 15:36:53 +0800

    feat: add user action

---
+++ dist/src/action/UserAction.js
@@ -0,0 +1,6 @@
+export var UserActionType;
+(function (UserActionType) {
+    UserActionType[UserActionType["LOGIN_BY_PHONE"] = 0] = "LOGIN_BY_PHONE";
+    UserActionType[UserActionType["USER_LOGIN"] = 1] = "USER_LOGIN";
+    UserActionType[UserActionType["GET_CURRENT_USER"] = 2] = "GET_CURRENT_USER";
+})(UserActionType || (UserActionType = {}));
