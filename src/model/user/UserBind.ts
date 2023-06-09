c0e784a6e47d38a8f784282ef57d2e4f04d7572a
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-06-09 12:49:17 +0800

    feat: add user bind info

---
+++ src/model/user/UserBind.ts
@@ -0,0 +1,6 @@
+export type UserBind = {
+    userId: number;
+    thirdUserId: number;
+    channelType: number;
+    id: number;
+};
--- src/model/user/UserModel.ts
+++ src/model/user/UserModel.ts
@@ -1,4 +1,7 @@
+import { UserBind } from "./UserBind";
+
 export type UserModel = {
     nickname: string;
     autoRenewProductExpireTimeMs: number;
+    thirdBind: UserBind[]
 }
