0a55022ee94b656e609926e0617286d17bd1de0c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-11 16:17:17 +0800

    feat: add random string generate function

--- src/utils/data/BaseMethods.ts
+++ src/utils/data/BaseMethods.ts
@@ -4,10 +4,10 @@ export const BaseMethods = {
      * @param params 
      * @returns 'String','Number'...
      */
-    getTypeOf: (params:any) => {
+    getTypeOf: (params: any) => {
         let type = Object.prototype.toString.call(params)
         let typeResult = type.match(/\[\w+\W(\w+)\]$/)
-        if(typeResult){
+        if (typeResult) {
             return typeResult[1]
         }
         return null
@@ -18,14 +18,14 @@ export const BaseMethods = {
      * @param AnyObject
      * @returns 
      */
-    isNull:(value:any) => {
+    isNull: (value: any) => {
         // any falsy value: https://developer.mozilla.org/en-US/docs/Glossary/Falsy
-        if (!value) {return true;}
+        if (!value) { return true; }
         if (typeof value === 'object') {
             // empty array
-            if (Array.isArray(value) && value.length === 0) {return true;}
+            if (Array.isArray(value) && value.length === 0) { return true; }
             // empty object
-            if (value.toString() === '[object Object]' && JSON.stringify(value) === '{}') {return true;}
+            if (value.toString() === '[object Object]' && JSON.stringify(value) === '{}') { return true; }
         }
         return false;
     },
@@ -34,19 +34,30 @@ export const BaseMethods = {
      * @param value 
      * @returns 
      */
-    isNumber:(value: string | number): boolean => {
+    isNumber: (value: string | number): boolean => {
         return ((value != null) &&
-           (value !== '') &&
-           !isNaN(Number(value.toString())));
+            (value !== '') &&
+            !isNaN(Number(value.toString())));
     },
     /**
      * 数组，字符串去重
      * @param Array,String
      * @returns 
      */
-    unique: function(params: any){
-        
-        
+    unique: function (params: any) {
+
+
+    },
+    genRandomStr: function (length: number): string {
+        let randomStr = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+            randomStr += characters.charAt(Math.floor(Math.random() * charactersLength));
+            counter += 1;
+        }
+        return randomStr;
     }
 }
 
